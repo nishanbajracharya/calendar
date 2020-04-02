f918f31eaa3638cfef28ae6613486d2adfbb971b
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-04-02 19:45:11 +0545

    Updates

---
+++ precache-manifest.c546e5a647ffdbdb5a8127760a7067ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3715a4078eedc54fa3588c91d7717c8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a59a1b378ddc6f34f4af",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ab15fdcadca4e64f0df9",
+    "url": "/static/css/main.4eb81824.chunk.css"
+  },
+  {
+    "revision": "a59a1b378ddc6f34f4af",
+    "url": "/static/js/2.78f8b385.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.78f8b385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab15fdcadca4e64f0df9",
+    "url": "/static/js/main.841c5cfb.chunk.js"
+  },
+  {
+    "revision": "43a1313c28a7d702b27c",
+    "url": "/static/js/runtime-main.a95642eb.js"
+  }
+]);
